b8da4d3900337bd33fedbd268042067a68956215
Takayuki Watanabe <watanabe@sikmi.com>
2023-08-30 13:21:53 +0900

    remote configで取得した値をview bindingでviewに反映

---
+++ app/src/main/java/com/google/firebase/quickstart/config/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.google.firebase.quickstart.config
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.quickstart.config.databinding.ActivityMainBinding
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.ktx.get
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.helloText.text = "hi! Michel."
+        val view = binding.root
+        setContentView(view)
+
+        // [START get_remote_config_instance]
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        // [END get_remote_config_instance]
+
+        // [START enable_dev_mode]
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 60
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        // [END enable_dev_mode]
+
+        // [START set_default_values]
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        // [END set_default_values]
+
+        // [START fetch_config_with_callback]
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+                    Toast.makeText(
+                        this,
+                        "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Fetch failed",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+                displayWelcomeMessage()
+            }
+        // [END fetch_config_with_callback]
+
+        // [START add_config_update_listener]
+        remoteConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) {
+                Log.d(TAG, "Updated keys: " + configUpdate.updatedKeys)
+
+                if (configUpdate.updatedKeys.contains("welcome_message")) {
+                    remoteConfig.activate().addOnCompleteListener {
+                        displayWelcomeMessage()
+                    }
+                }
+            }
+
+            override fun onError(error: FirebaseRemoteConfigException) {
+                Log.w(TAG, "Config update error with code: " + error.code, error)
+            }
+        })
+        // [END add_config_update_listener]
+    }
+
+    private fun displayWelcomeMessage() {
+        val remoteConfig = Firebase.remoteConfig
+
+        // [START get_config_values]
+        val welcomeMessage = remoteConfig[WELCOME_MESSAGE_KEY].asString()
+        // [END get_config_values]
+
+        Log.d(TAG, "Welcome message: $welcomeMessage")
+
+        binding.helloText.text = welcomeMessage
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+        // Remote Config keys
+        private const val WELCOME_MESSAGE_KEY = "welcome_message"
+    }
+}
